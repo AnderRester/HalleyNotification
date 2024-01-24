8c67155872a027d508e9b643ad16085a4eddac9a
AnderRester <anderrester@mail.ru>
2024-01-24 17:29:31 +0200

    Updates

--- actions.js
+++ actions.js
@@ -1,38 +1,184 @@
+const { BrowserWindow, Notification } = require("@electron/remote");
+const electron = require("electron");
+const fs = require("node:fs");
+require("dotenv").config();
+
 // Get Credentionals
-signInBtn = document.querySelector(".signInBtn");
+// console.log(process.env.userLogin);
+const signInBtn = document.querySelector(".signInBtn");
+const showPassBtn = document.querySelector(".showPassBtn");
+
 signInBtn.addEventListener("click", () => {
-    loginData = document.querySelector(".loginInput").value;
-    passwordData = document.querySelector(".passwordInput").value;
-
-    console.log(`Login: ${loginData}, Password: ${passwordData}`);
-});
-
-showPassBtn.addEventListener("click", () => {
-    passwordInput;
-});
-
-window
-    .open(
-        "https://www.halley.it//include/ihbxw.php?ente=H&amp;x=2d5a7596d0d3d5964ca8817dd36a98d8",
-        "modal",
-        "frame=false,nodeIntegration=no"
-    )
-    .then(() => {
-        document.addEventListener("DOMContentLoaded", () => {
-            enterUserData();
-        });
+    const loginData = document.querySelector(".loginInput").value;
+    const passwordData = document.querySelector(".passwordInput").value;
+    const userData = `userLogin="${loginData}" \nuserPassword="${passwordData}"`;
+    fs.writeFile(".env1", userData, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // file written successfully
+        }
     });
+    openMail(urlToOpen);
+});
 
-const enterUserData = () => {
-    let credentialsData = document.querySelectorAll("input");
-    credentialsData[5].value = "jiganov andrei";
-    credentialsData[6].value = "!!!Halley1";
-    document.querySelectorAll("button")[1].click();
+showPassBtn.addEventListener("mousedown", () => {
+    showPassBtn.style = "background-color: #acacac";
+    document.querySelector(".passwordInput").type = "text";
+});
+showPassBtn.addEventListener("mouseup", () => {
+    document.querySelector(".passwordInput").type = "password";
+    showPassBtn.style = "background-color: #333333";
+});
+
+const urlToOpen =
+    "https://www.halley.it//include/ihbxw.php?ente=H&amp;x=2d5a7596d0d3d5964ca8817dd36a98d8";
+const openMail = (url) => {
+    const openedWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            webSecurity: false,
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        },
+    });
+    openedWindow.webContents.on("before-input-event", (event, input) => {
+        if (input.control && input.key.toLowerCase() === "i") {
+            console.log("Pressed Control+I");
+            openedWindow.webContents.openDevTools();
+            event.preventDefault();
+        }
+    });
+    openedWindow.setMenu(null);
+    openedWindow.loadURL(url);
+
+    let NOTIFICATION_TITLE = "You've got message";
+    let NOTIFICATION_BODY = new Date().toLocaleTimeString();
+
+    var data = [];
+    let counter = 0;
+    const login = process.env.userLogin;
+    const password = process.env.userPassword;
+
+    openedWindow.webContents.on("did-finish-load", () => {
+        let code = ``;
+        if (counter == 2) {
+            code = `
+        const enterUserData = () => {
+            let credentialsData = document.querySelectorAll("input");
+            credentialsData[5].value = "jiganov andrei";
+            credentialsData[6].value = "!!!Halley1";
+            // credentialsData[5].value = ${login};
+            // credentialsData[6].value = ${password};
+            document.querySelectorAll("button")[1].click();
+        };
+        enterUserData();
+        `;
+            openedWindow.webContents.executeJavaScript(code);
+            counter++;
+        } else {
+            counter++;
+        }
+        const scanMailBox = setInterval(() => {
+            if (counter >= 5) {
+                console.log(counter);
+                code = `
+            const getUnreadedMessages = () => {
+                    
+                    let messages = document.querySelectorAll(".HR_bold .hListHDataContainer");
+                    if (messages != undefined) {
+                        // for (let i = 0; i < messages.length; i += 8) {
+                        //     try {
+                        //         console.log(messages[4 + i].innerText);
+                        //         console.log(messages[5 + i].innerText);
+                        //         // let NOTIFICATION_TITLE = messages[4 + i].innerText";
+                        //         // let NOTIFICATION_BODY = messages[5 + i].innerText;
+                        //         // new Notification({
+                        //         //     title: NOTIFICATION_TITLE,
+                        //         //     body: NOTIFICATION_BODY,
+                        //         // }).show();
+                        //     } catch {
+                        //         console.log("No Unreaded Messages");
+                        //     }
+                        // }
+                        ${new Notification({
+                            title: NOTIFICATION_TITLE,
+                            body: NOTIFICATION_BODY,
+                        }).show()}
+                    }
+                
+            };
+
+            const stopScan = () => {
+                clearInterval(scanMailBox);
+            };
+            
+            getUnreadedMessages();
+        `;
+            }
+        }, 9000);
+        counter++;
+
+        openedWindow.webContents.executeJavaScript(code);
+    });
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-    enterUserData();
-});
+/*
+
+const enterUserData = () => {
+                // console.log(item.document);
+                let credentialsData = document.querySelectorAll("input");
+                credentialsData[5].value = "jiganov andrei";
+                credentialsData[6].value = "!!!Halley1";
+                document.querySelectorAll("button")[1].click();
+            };
+
+            const getUnreadedMessages = () => {
+                let messages = document.querySelectorAll(".HR_bold .hListHDataContainer");
+                if (messages != undefined) {
+                    for (let i = 0; i < messages.length; i += 8) {
+                        try {
+                            console.log(messages[4 + i].innerText);
+                            console.log(messages[5 + i].innerText);
+                        } catch {
+                            console.log("No Unreaded Messages");
+                        }
+                    }
+                }
+                const scanMailBox = setInterval(() => {
+                    let messages = document.querySelectorAll(".HR_bold .hListHDataContainer");
+                    if (messages != undefined) {
+                        for (let i = 0; i < messages.length; i += 8) {
+                            try {
+                                console.log(messages[4 + i].innerText);
+                                console.log(messages[5 + i].innerText);
+                            } catch {
+                                console.log("No Unreaded Messages");
+                            }
+                        }
+                    }
+                }, 600000);
+            };
+            
+            const stopScan = () => {
+                clearInterval(scanMailBox);
+            };
+
+            enterUserData();
+            getUnreadedMessages();
+
+
+*/
+
+// const enterUserData = () => {
+//     // console.log(item.document);
+//     let credentialsData = document.querySelectorAll("input");
+//     credentialsData[5].value = "jiganov andrei";
+//     credentialsData[6].value = "!!!Halley1";
+//     document.querySelectorAll("button")[1].click();
+// };
 
 // let credentialsData = document.querySelectorAll("input");
 // credentialsData[5].value = "jiganov andrei";
@@ -42,11 +188,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // 4 12 Sender
 // 5 13 Message TOTAL 16 Elements
-
-setInterval(() => {
-    console.log("clear");
-}, 10000);
-
 /*
 
 unreadedMessages[4].innerText
@@ -60,4 +201,89 @@ unreadedMessages[5+16].innerText
 
 */
 
-const getUnreadedMessages = () => {};
+// const getUnreadedMessages = () => {
+//     let messages = document.querySelectorAll(".HR_bold .hListHDataContainer");
+//     if (messages != undefined) {
+//         for (let i = 0; i < messages.length; i += 8) {
+//             try {
+//                 console.log(messages[4 + i].innerText);
+//                 console.log(messages[5 + i].innerText);
+//             } catch {
+//                 console.log("No Unreaded Messages");
+//             }
+//         }
+//     }
+//     const scanMailBox = setInterval(() => {
+//         let messages = document.querySelectorAll(".HR_bold .hListHDataContainer");
+//         if (messages != undefined) {
+//             for (let i = 0; i < messages.length; i += 8) {
+//                 try {
+//                     console.log(messages[4 + i].innerText);
+//                     console.log(messages[5 + i].innerText);
+//                 } catch {
+//                     console.log("No Unreaded Messages");
+//                 }
+//             }
+//         }
+//     }, 600000);
+// };
+
+// const stopScan = () => {
+//     clearInterval(scanMailBox);
+// };
+
+/*
+
+const enterUserData = (item) => {
+    let credentialsData = document.querySelectorAll("input");
+    credentialsData[5].value = "jiganov andrei";
+    credentialsData[6].value = "!!!Halley1";
+    document.querySelectorAll("button")[1].click();
+};
+
+const scanMailBox = setInterval(() => {
+        let messages = document.querySelectorAll(".HR_bold .hListHDataContainer");
+        if (messages != undefined) {
+            for (let i = 0; i < messages.length; i += 8) {
+                try {
+                    console.log(messages[4 + i].innerText);
+                    console.log(messages[5 + i].innerText);
+                } catch {
+                    console.log("No Unreaded Messages");
+                }
+            }
+        }
+    }, 600000);
+
+const stopScan = () => {
+    clearInterval(scanMailBox);
+
+};
+
+*/
+
+/*
+
+let messages = document.querySelectorAll(".HR_bold .hListHDataContainer");
+    if (messages != undefined) {
+        for (let i = 0; i < messages.length; i += 8) {
+            try {
+                console.log(messages[4 + i].innerText);
+                console.log(messages[5 + i].innerText);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+
+for (let i = 0; i <= messages.length; i += 8) {
+                try {
+                    if (i + 8 >= messages.length) break;
+                    console.log(messages[4 + i].innerText);
+                    console.log(messages[5 + i].innerText);
+                } catch {
+                    console.log("No Unreaded Messages");
+                }
+            }
+
+*/
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const { app, BrowserWindow, Notification } = require("electron");
-
+const { app, BrowserWindow, Notification, ipcMain } = require("electron");
+require("@electron/remote/main").initialize();
 // App Window Settings
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        webPreferences: {
+            webSecurity: false,
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
     });
     win.setMenu(null);
-
+    win.webContents.openDevTools();
+    require("@electron/remote/main").enable(win.webContents);
     win.webContents.on("before-input-event", (event, input) => {
         if (input.control && input.key.toLowerCase() === "i") {
             console.log("Pressed Control+I");
@@ -17,22 +23,46 @@ const createWindow = () => {
         }
     });
 
+    ipcMain.on("getNotification", (title) => {});
+
+    // win.webContents.executeJavaScript(``)
+
+    // win.loadURL(
+    //     "https://www.halley.it//include/ihbxw.php?ente=H&amp;x=2d5a7596d0d3d5964ca8817dd36a98d8"
+    // );
+
     win.loadFile("index.html");
 
-    // win.webContents.setWindowOpenHandler(() => {
-    //     shell.openExternal(
-    //         "https://www.halley.it//include/ihbxw.php?ente=H&amp;x=2d5a7596d0d3d5964ca8817dd36a98d8"
-    //     ); // Open URL in user's browser.
-    //     return { action: "deny" }; // Prevent the app from opening the URL.
+    // win.webContents.setWindowOpenHandler((url) => {
+    //     if (url === "about:blank") {
+    //         return {
+    //             action: "allow",
+    //             overrideBrowserWindowOptions: {
+    //                 frame: true,
+    //                 fullscreenable: false,
+    //                 backgroundColor: "black",
+    //                 webPreferences: {
+    //                     webSecurity: false,
+    //                 },
+    //             },
+    //         };
+    //     }
+    //     console.log(url);
+    //     return { action: "allow" };
     // });
+    // win.webContents.openDevTools();
 };
-const NOTIFICATION_TITLE = "Application Has Been Launched";
-const NOTIFICATION_BODY = "Notification from the Main process";
+
+// Notifications
+
+// const NOTIFICATION_TITLE = "Application Has Been Launched";
+// const NOTIFICATION_BODY = "Notification from the Main process";
 
 app.whenReady().then(() => {
     createWindow();
-    new Notification({
-        title: NOTIFICATION_TITLE,
-        body: NOTIFICATION_BODY,
-    }).show();
+    // app.commandLine.appendSwitch("disable-features", "OutOfBlinkCors");
+    // new Notification({
+    //     title: NOTIFICATION_TITLE,
+    //     body: NOTIFICATION_BODY,
+    // }).show();
 });
